7e9251e231bc492fd72f20e653a34732b5d16903
Mariana Santos <138501950+marianasantosc@users.noreply.github.com>
2023-07-11 13:53:19 -0300

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt("#animacaoSimples", {
+        strings: "UX Researcher - Criadora de conteúdo - Instrutora - Empreendedora",
+        speed: 50,
+        waitUntilVisible: true,
+      }).go();
+})
